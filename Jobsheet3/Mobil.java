35da993e2b8ede58c779850f380f107daea96309
= <kiminsetyawan@gmail.com>
2025-09-10 10:50:35 +0700

    Soal no. 2

--- Jobsheet3/Mobil.java
+++ Jobsheet3/Mobil.java
@@ -3,28 +3,26 @@ package Jobsheet3;
 public class Mobil {
     private String merek = "Toyota";
     private int kecepatan;
-    // deklarasi atribut menggunakan private
 
-    public void setMerek(String merek) {
-        this.merek = merek;
-    } // berfungsi untuk memanipulasi nilai dari atribut private(merek)
+    // public void setMerek(String merek) {
+        // this.merek = merek;
+    // }
     
     public String getMerek() {
         return merek;
-    }  // berfungsi mengembalikan nilai dari atribut private(merek)
+    }  
 
     public void setKecepatan(int kecepatan) {
         this.kecepatan = kecepatan;
-    } // berfungsi untuk memanipulasi nilai dari atribut private(kecepatan)
+    } 
 
-     public int getKecepatan() {
-        return kecepatan;
-    }  // berfungsi mengembalikan nilai dari atribut private(kecepatan)
+    // public int getKecepatan() {
+        // return kecepatan;
+    // } 
 
     public void tampilkanInfo() {
         System.out.println("Merek Mobil: " + merek);
-        System.out.println("Kecepatan: " + kecepatan + " km/jam");
-        // untuk menampilkan merek dan kecepatan
+        // System.out.println("Kecepatan: " + kecepatan + " km/jam");
     }
 }
 
