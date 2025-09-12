301bcd95f72f3f8966283d0550d4e6a0b1f501b8
= <kiminsetyawan@gmail.com>
2025-09-12 09:34:22 +0700

    Percobaan 3

--- Jobsheet3/Motor.java
+++ Jobsheet3/Motor.java
@@ -26,9 +26,10 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-        if (!this.isMesinOn && kecepatan > 0 && kecepatan < 100) {
+        if (!this.isMesinOn && kecepatan > 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
         }
+        
         else {
             this.kecepatan = kecepatan;
         }
