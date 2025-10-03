dd819afd29d1ba812c85c8cfc69f8a888c0c8432
= <kiminsetyawan@gmail.com>
2025-10-03 10:11:19 +0700

    Langkah 2

--- Jobsheet6/InheritanceDemo.java
+++ Jobsheet6/InheritanceDemo.java
@@ -1,13 +1,7 @@
 public class InheritanceDemo {
 
     public static void main(String[] args) {
-        Dosen dosen1 = new Dosen();
-
-        dosen1.nama = "Zaki";
-        dosen1.nip = "1234";
-        dosen1.gaji = 3000000;
-        dosen1.nidn = "5678";
-
+        Dosen dosen1 = new Dosen("123", "Zaki", 3000000, "456");
         System.out.println(dosen1.getAllInfo());
     }
 }
