b367b79ef183f531f5046be20cb33a212d631def
= <kiminsetyawan@gmail.com>
2025-10-03 10:12:43 +0700

    Percobaan 7 langkah 1

--- Jobsheet6/Dosen.java
+++ Jobsheet6/Dosen.java
@@ -2,7 +2,10 @@ public class Dosen extends Pegawai {
     public String nidn;
 
     public Dosen(String nip, String nama, double gaji, String nidn) {
-        System.out.println("Objek dari class Dosen dibuat");
+        this.nip = nip;
+        this.nama = nama;
+        this.gaji = gaji;
+        this.nidn = nidn;
     }
 
     public String getInfo() {
