f1b49178a0c3932378a72f9bcedb057783464c27
= <kiminsetyawan@gmail.com>
2025-10-03 10:14:41 +0700

    Langkah 4

--- Jobsheet6/Pegawai.java
+++ Jobsheet6/Pegawai.java
@@ -3,8 +3,10 @@ public class Pegawai {
     public String nama;
     public double gaji;
 
-    public Pegawai() {
-        System.out.println("Objek dari class Pegawai dibuat");
+    public Pegawai(String nip, String nama, double gaji) {
+        this.nip = nip;
+        this.nama = nama;
+        this.gaji = gaji;
     }
 
     public String getInfo() {
